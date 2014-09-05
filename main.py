0e78646f7e3d3b4e7fc17da776a28a2a2ed44fdb
tsubasa <nor.known.to.life.51122@gmail.com>
2014-09-05 17:13:27 +0900

    amazon商品リンク取得

--- main.py
+++ main.py
@@ -31,8 +31,10 @@ class Word(ndb.Model):
   member_id = ndb.IntegerProperty()
   word = ndb.StringProperty()
   hiragana = ndb.StringProperty()
-  amazonlink = ndb.StringProperty()
+  image_url = ndb.StringProperty()
+  amazon_link = ndb.StringProperty()
   created = ndb.DateTimeProperty(auto_now_add=True)
+
 
 class MainPage(webapp2.RequestHandler):
 
@@ -46,19 +48,31 @@ class MainPage(webapp2.RequestHandler):
     self.response.write(template.render(template_values))
 
   def post(self):
+    # しりとらずの失敗判定の初期化
+    isFailed = False
+
+    # IDのオートインクリメント
+    post_count = Word.query().count()
+    next_id = post_count + 1
+
     # テキストフィールドのワード取得
     post_word = self.request.get('word')
 
-    # オートインクリメント
-    next_id = Word.query().count() + 1
+    # ワードのひらがな変換
+    hiragana = yahoo.reading(post_word)
+
+    # しりとらず失敗判定
+    # if hiragana[len(hiragana)-1] == Word.query(word_id=post_count):
+    #   isFalied = True
 
     image_url = ''
+    amazon_link = ''
 
     # 503エラー対策
     retry_count = 0
     while retry_count < 5:
       try:
-        res = config.amazon.ItemSearch(Keywords=post_word, SearchIndex='All', ItemPage='1', ResponseGroup="Images")
+        res = config.amazon.ItemSearch(Keywords=post_word, SearchIndex='All', ItemPage='1', ResponseGroup="Medium")
       except:
         retry_count += 1
         time.sleep(1)
@@ -68,16 +82,24 @@ class MainPage(webapp2.RequestHandler):
     # xml切り出し
     root = fromstring(res)
     Items = root.find(xmlns + 'Items')
-    if len(Items):
-      Item = Items.findall(xmlns + 'Item')
-      if len(Item):
-        ImageSets = Item[0].find(xmlns + 'ImageSets')
-        ImageSet = ImageSets.find(xmlns + 'ImageSet')
-        MediumImage = ImageSet.find(xmlns + 'MediumImage')
-        image_url = MediumImage.findtext(xmlns + 'URL')
+    TotalResults = Items.findtext(xmlns + 'TotalResults')
+    if int(TotalResults) != 0:
+      if len(Items):
+        Item = Items.findall(xmlns + 'Item')
+        if len(Item):
+          ImageSets = Item[0].find(xmlns + 'ImageSets')
+          ImageSet = ImageSets.find(xmlns + 'ImageSet')
+          MediumImage = ImageSet.find(xmlns + 'SmallImage')
+          image_url = MediumImage.findtext(xmlns + 'URL')
+          amazon_link = Item[0].findtext(xmlns + 'DetailPageURL')
+    else:
+      # しりとらず失敗
+      isFailed = True
 
-    word = Word(word_id=next_id, member_id=1, word=post_word, hiragana=yahoo.reading(post_word), amazonlink=image_url)
-    word.put()
+    if not isFailed:
+      word = Word(word_id=next_id, member_id=0, word=post_word, hiragana=hiragana, image_url=image_url, amazon_link=amazon_link)
+      word.put()
+
     self.redirect('/')
 
 

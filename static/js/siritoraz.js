1825a1a0a3453df27518f3b27165268144ef683a
tsubasa <nor.known.to.life.51122@gmail.com>
2014-09-10 16:16:20 +0900

    クッキー管理と、1段階目の機能解放実装

--- static/js/siritoraz.js
+++ static/js/siritoraz.js
@@ -18,6 +18,11 @@ $(document).ready(function() {
 		addWord(words[i]);
 	}
 
+	// 初訪問時のクッキー初期化
+	initCookies();
+	// クッキーの機能解放状態による画面表示非表示の変更
+	functionRelease(getCookie("releaseStatus"));
+
 	// 鯖接続開始
 	var channel = new goog.appengine.Channel($("#token").val());
 	var socket = channel.open({
@@ -37,7 +42,7 @@ $(document).ready(function() {
 				setTimeout(function() {
 					// タイトル消去
 					$("#carousel .carousel-inner > div").first().remove();
-				
+
 					// TODO スライドボタン追加
 				}, 2000);
 
@@ -97,26 +102,35 @@ $(document).ready(function() {
 		// new_word 更新
 		new_word = data;
 
+		// クッキーの投稿回数を1回増やす
+		addPostCount(parseInt(getCookie("postCount")));
+
+		// 機能解放の判定
+		functionRelease(getCookie("releaseStatus"), getCookie("postCount"));
+
+		// 機能解放状態による画面表示非表示の変更
+		changeVisibility(getCookie("releaseStatus"));
+
 		enableSubmit = true;
 	}
 
 	// 過去ワードリストに前のワードを追加
 	function addWord(data) {
 		$("tbody").prepend("<tr>" +
-			"<td>" + data.word_id + "</td>" + 
+			"<td>" + data.word_id + "</td>" +
 			"<td>" + data.word +
-			"（" + data.hiragana + "）</td>" + 
-			"<td>" + "<a href=" + data.amazon_link + 
-			"><img src=" + data.image_url + ' style="height : 30px;" alt="アマゾンの画像リンク"></a></td>"' + 
-			"<td>" + data.created_at + "</td>" + 
+			"（" + data.hiragana + "）</td>" +
+			"<td>" + "<a href=" + data.amazon_link +
+			"><img src=" + data.image_url + ' style="height : 30px;" alt="アマゾンの画像リンク"></a></td>"' +
+			"<td>" + data.created_at + "</td>" +
 			"</tr>");
 	}
 
 	// カルーセルにワードを追加
 	function addCarouselWord(data) {
 		$("#carousel .carousel-inner").append(
-			'<div class="item"><span class="word">' + 
-			data.word + '</span><br><span class="hiragana">' + 
+			'<div class="item"><span class="word">' +
+			data.word + '</span><br><span class="hiragana">' +
 			data.hiragana + '</span>');
 	}
 
@@ -127,5 +141,74 @@ $(document).ready(function() {
 		$("#word_submit").addClass("has-error");
 
 		enableSubmit = true;
+	}
+
+	// 指定した要素のクッキーデータを取得
+	function getCookie(name) {
+		var result = null;
+
+		var cookieName = name + '=';
+		var allcookies = document.cookie;
+
+		var position = allcookies.indexOf(cookieName);
+		if(position != -1) {
+			var startIndex = position + cookieName.length;
+
+			var endIndex = allcookies.indexOf(';', startIndex);
+			if(endIndex == -1) {
+				endIndex = allcookies.length;
+			}
+
+			result = decodeURIComponent(allcookies.substring(startIndex, endIndex));
+		}
+
+		return result;
+	}
+
+	// 初訪問時のクッキー初期化
+	function initCookies() {
+		if(!getCookie("releaseStatus")) {
+			console.log("releaseStatus!");
+			document.cookie = "releaseStatus=0";
+		}
+
+		if(!getCookie("postCount")) {
+			console.log("postCount!");
+			document.cookie = "postCount=0";
+		}
+
+		if(!getCookie("lastPostTime")) {
+			console.log("lastPostTime!");
+			document.cookie = "lastPostTime=0";
+		}
+	}
+
+	// 機能解放の判定
+	function functionRelease(status, count) {
+		// 初めての投稿
+		if(status == "0" && count == "1") {
+			document.cookie = "releaseStatus=1";
+		}
+	}
+
+	// 機能解放状態による画面表示非表示の変更
+	function changeVisibility(status) {
+		if(parseInt(status) > 0) {
+			$("#affiliate").css("display", "block");
+
+			if(parseInt(status) > 1) {
+				$("#carousel_bottun").css("visibility", "visible");
+
+				if(parseInt(status) > 2) {
+					$("#past_wordlist").css("display", "block");
+				}
+			}
+		}
+	}
+
+	// 投稿回数を増やす
+	function addPostCount(count) {
+		count++;
+		document.cookie = "postCount=" + encodeURIComponent(String(count));
 	}
 });

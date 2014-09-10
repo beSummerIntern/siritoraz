5e6cd08fbec01400f7f3d49d41028346f6c4578e
ほつれ <cerceris326@gmail.com>
2014-09-10 12:38:08 +0900

    カルーセル機能が半分くらい完成

--- static/js/siritoraz.js
+++ static/js/siritoraz.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	// 変数
+	var enableSubmit = false;
+
 	// 初期処理
 	// 初期データをJSONに変換、配列に追加
 	var words = [];
@@ -7,34 +10,50 @@ $(document).ready(function() {
 		$(this).remove();
 	});
 
+	// 今のワードを保持
 	var new_word = words[0];
 
 	// 過去のワードリストを生成
-	var words_length = words.length;
-	for (var i = 1; i < words.length; i++) {
-		$("tbody").append("<tr>" +
-			"<td>" + words[i].word_id + "</td>" + 
-			"<td>" + words[i].word +
-			" （" + words[i].hiragana + "）</td>" +  
-			"<td>" + "<a href=" + words[i].amazon_link + 
-			"><img src=" + words[i].image_url + ' style="height : 30px;" alt="アマゾンの画像リンク"></a></td>"' + 
-			"<td>" + words[i].created_at + "</td>" + 
-			"</tr>");
+	for (var i = words.length - 1; i > 0; i--) {
+		addWord(words[i]);
 	}
 
 	// 鯖接続開始
 	var channel = new goog.appengine.Channel($("#token").val());
 	var socket = channel.open({
+		// 鯖接続完了時
 		onopen : function(){
+			// カルーセル編集
+			for (var i = words.length - 1; i >= 0; i--) {
+				addCarouselWord(words[i]);
+			}
+
+			// タイトル処理
+			setTimeout(function() {
+				// タイトルをスライド
+				$("#carousel").carousel("prev");
+
+				// スライド後処理
+				setTimeout(function() {
+					// タイトル消去
+					$("#carousel .carousel-inner > div").first().remove();
+				
+					// TODO スライドボタン追加
+				}, 2000);
+
+				enableSubmit = true;
+			}, 1000);
 		},
 
+		// メッセージ受信
 		onmessage : function(message) {
 			var data = JSON.parse(message.data);
 
+			// メッセージタイプ識別
 			if(data.type == "error") {
 				dispError(data.error_message);
 			} else if (data.type == "new_word") {
-				addWord(data);
+				changeCurrentWord(data);
 			}
 		},
 
@@ -47,14 +66,15 @@ $(document).ready(function() {
 
 	// ワード投稿時
 	$("#word_submit").submit(function(){
-		// データ送信
-		$.post("/", $(this).serialize());
+		if (enableSubmit) {
+			$.post("/", $(this).serialize());
+		}
 
 		return false;
 	});
 
 	// 今のワード変更時
-	function addWord(data) {
+	function changeCurrentWord(data) {
 		// エラーメッセージ除去、更新
 		$("#word_submit .help-block").text("しりとらず成功！");
 		$("#word_submit").removeClass("has-error");
@@ -63,25 +83,38 @@ $(document).ready(function() {
 		$("#word_submit input[name='word']").val("");
 
 		// 表示の更新
-		$("#screen_word").text(data.word);
-		$("#screen_hiragana").text(data.hiragana);
+		addCarouselWord(data);
+		$("#carousel").carousel("next");
 
 		$("#affiliate span").text(data.word);
 		$("#affiliate a").attr("href", data.amazon_link);
 		$("#affiliate img").attr("src", data.image_url);
 
 		// 過去ワードリストに前のワードを追加
-		$("tbody").prepend("<tr>" +
-			"<td>" + new_word.word_id + "</td>" + 
-			"<td>" + new_word.word +
-			"（" + new_word.hiragana + "）</td>" + 
-			"<td>" + "<a href=" + new_word.amazon_link + 
-			"><img src=" + new_word.image_url + ' style="height : 30px;" alt="アマゾンの画像リンク"></a></td>"' + 
-			"<td>" + new_word.created_at + "</td>" + 
-			"</tr>");
+		addWord(new_word);
 
 		// new_word 更新
 		new_word = data;
+	}
+
+	// 過去ワードリストに前のワードを追加
+	function addWord(data) {
+		$("tbody").prepend("<tr>" +
+			"<td>" + data.word_id + "</td>" + 
+			"<td>" + data.word +
+			"（" + data.hiragana + "）</td>" + 
+			"<td>" + "<a href=" + data.amazon_link + 
+			"><img src=" + data.image_url + ' style="height : 30px;" alt="アマゾンの画像リンク"></a></td>"' + 
+			"<td>" + data.created_at + "</td>" + 
+			"</tr>");
+	}
+
+	// カルーセルにワードを追加
+	function addCarouselWord(data) {
+		$("#carousel .carousel-inner").append(
+			'<div class="item"><span class="word">' + 
+			data.word + '</span><br><span class="hiragana">' + 
+			data.hiragana + '</span>');
 	}
 
 	// エラーメッセージ表示
